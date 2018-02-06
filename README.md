7f5769ce23efa5dda7277e7714a4cf1efc4a7961
Dan Dumitrache <vietnamezul@gmail.com>
2018-02-06 04:31:49 +0000

    day 21 of bootcamp, stream 2 - day 1, back end development with Python, 56 challenges

---
+++ README.md
@@ -0,0 +1,4 @@
+Day 21 of the bootcamp
+First day of backend development with Python
+56 Python challenges to solve
+All the code in the trypython.py file is comented out
---
+++ trypython.py
@@ -0,0 +1,187 @@
+# print('Hello World')
+# print('Welcome To Python')
+
+# name = input("Enter name: ")
+# print('Hello ' + name)
+
+# num1 = int(input('Enter a number: '))
+# num2 = int(input('Enter another number: '))
+# result = num1 + num2
+# print(num1, ' plus ', num2, ' = ', result)
+
+# s = input("Enter some text: ")
+# print(s.upper())
+# print(s.lower())
+# print(s.title())
+
+# myString = 'This is just a string'
+# reversedString = myString[::-1]
+# print(reversedString)
+
+# numberOne = int(input("Enter a number: "))
+# if numberOne <= 10:
+#     print("small")
+# else:
+#     print("big")
+
+# numOne = int(input("Enter a number: "))
+# if numOne == 1:
+#     nameOne = input("Enter your name: ")
+#     print("Your name is " + nameOne)
+# elif numOne == 2:
+#     ageOne = int(input("Enter your age: "))
+#     print("Your age is ", ageOne)
+# else:
+#     print("Type 1 or 2 please")
+
+# n1 = int(input("type the first number"))
+# n2 = int(input("type the second number"))
+# n3 = int(input("type the third number"))
+# n4 = int(input("type the forth number"))
+# n5 = int(input("type the fifth number"))
+
+# n6 = int(input("type the sixth number"))
+# n7 = int(input("type the seveth number"))
+# n8 = int(input("type the eight number"))
+# n9 = int(input("type the ninth number"))
+# n10 = int(input("type the tenth number"))
+
+# result = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10
+
+# print(result)
+
+# result = []
+# for n in range(3):
+#     number = int(input("enter a number: "))
+#     result.append(number)
+# print(result)
+
+# print(max(result))
+
+# 1. Create a blank C9 workspace called trypython
+# 2. What version of python are you running in the workspace?
+# 3. Check if python 3 is installed in the workspace
+# 4. what version of python 3 is installed?
+# 5. What is a REPL?
+# 6. Run the python 3 interpreter
+# 7. Evaluate the expression 10 + 10
+# 8. Evaluate expressions using each of the mathematical operators +, -, *, /
+# 9. Use parenthesis to change the order than an expression is evaluated
+# 10. What is the power operator?
+# 11. Calculate 2 to the power of 8.
+# 12. Create a variable x, with a value 10
+# 13. Evaluate the expression x
+# 14. Evaluate the expression x * 2
+# 15. Evaluate the expression x to the power of x
+# 16. What is the 'type' of variable x?
+# 17. Create a variable s with a value of 'Hello'
+# 18. Evaluate the expression s * 5
+# 19. What is the 'type' of variable s
+# 20. Create a variable t with a value of 'World'
+# 21. Evaluate the expression s + t
+# 22. Create an expression using the variables s and t that places a space between the words.
+# 23. Create a variable a with a value of True
+# 24. Create a variable b with a value of False
+# 25. Evaluate the expression a and b
+# 26. Evaluate the expression a or b
+# 27. Evalate the expression not a
+# 28. Evalate the expression not b
+# 29. Explain how the boolean expressions above are evaluated.
+# 30. Write out the truth table for the and operator
+# 31. Write our the truth table for the or operator
+# 32. Write our the truth table for the not operator
+# 33. Is the following Truth Table correct, or incorrect? Explain
+# a    b    (not a) or b
+# T    T    T
+# T    F    F
+# F    T    T
+# F    F    F
+# 34. Assuming p and q are boolean expressions, Can the following expression be
+# simplified? Explain
+# Clue: Write out the Truth Table.
+# (p and q) or p
+# 35. Exit the Python Interpreter
+# a    b    (not a) or b
+# T    T    T
+# T    F    F
+# F    T    T
+# F    F    *T*
+# 36. Create a python script called trypython.py, containing the following code
+# print('Hello World')
+# print('Welcome To Python') (edited)
+# 37. Execute the python script trypython.py from the command line.
+# 38. Write a python program that asks the user to enter their name, allows them to enter text,
+# and then say's hello as follows:
+# Enter Name: Richard
+# Hello Richard
+# 39. Write a program that asks the user to enter two numbers, the prints the result of adding them.
+# Enter First Number: 3
+# Enter Second Number: 5
+# 3 plus 5 = 8
+# 40. Write program that allows the user to enter some text.
+# Display that text in all UPPER CASE
+# 41. Write program that allows the user to enter some text.
+# Display that text in all lower case
+# 42. Write program that allows the user to enter some text.
+# Display that text in Title Case
+# 43. Write a program that allows the user to enter some text.
+# Display the text reversed.
+# 44. Write a program that allows the user to input a number.
+# If the number is 10 or less, print "Small"
+# otherwise, print "Big"
+# 45. Write a program that allows the user to input two numbers.
+# If the numbers are the same, print "Same"
+# otherwise print "Different"
+# 46. Write a program that allows the user to enter a number.
+# If the number is 1, ask the user to enter their name e.g. Tom, and print
+# Your Name is Tom
+# If the number entered was 2, ask the user to enter their age, e.g. 21 and print
+# Your Age is 21
+# 47. Write a program that allows the user to enter 10 numbers. Display the sum of them all.
+# 48. Write a program that allows the user to enter 10 numbers. Display the maximum of them all.
+# 49. Write a program that uses a FOR loop to iterate over a range of numbers from 0 to 10 and prints them.
+# 50. If not done already, modify program 48 to use a FOR loop.
+# 51. If not done already, modify program 49 to use a FOR loop.
+# 52. Write a program that asks the user to enter a number.
+#   Loop that many times, asking the user to enter a number
+# 53. Write a script that contains a function called add, which accepts two arguments a, and b.
+# The function should return the result of adding a and b. In the script, call the function
+# passing it 3 and 5, and print the result.
+
+# def add(a, b):
+#     result = a + b
+#     print(result)
+# add(3, 5)
+
+# 54. Modify the script above to allow the user to enter two numbers. Pass those numbers to
+# the add function, print the result.
+
+# 55. Write a function that accepts one argument n. You can assume that n is an integer.
+# The function should return True if n is even, False if it's odd.
+
+# 56. Write a function called message that accepts no arguments, it returns "This is the message". Try the following two print
+# statements to test the function, compare the results.
+# with_parens = message()
+# without_parens = message
+
+
+
+# print(with_parens)
+
+# print(without_parens)
+
+# a = int(input("type a number: "))
+# b = int(input("type another number: "))
+# def add(a, b):
+#     result = a + b
+#     print(result)
+# add(a, b)
+
+# num = int(input("Type a number: "))
+
+# def myFunction(num):
+#     if num % 2 == 0:
+#         return True
+#     else:
+#         return False
+# print(myFunction(num))
